88eac325ca51f6d4525dc10ee6eb3ca3d2d18496
roohinkukreja02 <roohinkukreja@gmail.com>
2024-09-30 03:26:44 +0530

    codeissance

---
+++ index.js
@@ -0,0 +1,44 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const port=6006;
+const app=express();
+const session=require("express-session");
+
+//parsing body and using in functions
+app.use(express.json());
+//allows parsing of more objects, more than key value pairs
+app.use(express.urlencoded({extended: true}));
+
+//for public folder
+app.use(express.static('public'));
+//for ejs files
+app.set("view engine", "ejs");
+
+//mongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/myproject")
+.then(console.log("MongoDB connected"))
+.catch((err)=>{console.log(err)});
+
+
+app.use(session({
+    secret: 'your_secret_key', // Replace with a strong secret key    
+    resave: false,
+    saveUninitialized: true,
+   
+
+    cookie: {
+      maxAge: 3600000,  // 1 hour
+      httpOnly: true,   // Secure cookie
+      secure: false     // Use true in production with HTTPS
+  }
+  }));
+
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`);
+});
